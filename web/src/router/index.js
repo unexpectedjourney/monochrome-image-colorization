a9d7471bce2c3017041c2a1ea1404378dac07845
Anton Babenko <antonwarbeck123@gmail.com>
2020-04-20 18:19:56 +0300

    Added upload page for web

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -14,6 +14,11 @@ const routes = [
         path: '/about',
         name: 'About',
         component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/upload',
+        name: 'ImageUpload',
+        component: () => import(/* webpackChunkName: "about" */ '../views/ImageUpload.vue')
     }
 ]
 
